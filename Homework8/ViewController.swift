cffe592eb8184739df8e8b5a2c7028e7c24c3bb8
anto004 <anto_004@live.com>
2018-09-05 16:17:18 -0700

    initial commit

--- Homework8/ViewController.swift
+++ Homework8/ViewController.swift
@@ -8,6 +8,21 @@
 
 import UIKit
 
+/**
+ API key: AIzaSyAGwOW2z3SGXjO6n9VoEjmEnIxdNM7paTU
+ search eg:
+ https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=34.119193,-118.112650&radius=1500&type=restaurant&keyword=italian&key=AIzaSyAGwOW2z3SGXjO6n9VoEjmEnIxdNM7paTU
+ 
+ Yelp
+ API key: ssrhCpzL11gRI-IHI4sMw9gT09tSMSxnJQdhcRs5jtEIY8tp6j8zV0YAZWsyuUCWeSw4MGRcN_828M-8I7Lu2J2cHlOqf1iOvRBww90RTAEZnsa7ZqPssg8_H1yQW3Yx
+ 
+ Client ID:
+ Rh91giys8kpc4qrWHkewVA
+ 
+ search eg:
+    GET https://api.yelp.com/v3/businesses/search?term=restaurant&latitude=34.119193&longitude=-118.112650
+**/
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
