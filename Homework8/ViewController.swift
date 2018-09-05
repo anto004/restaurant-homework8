842788d157962b2a32a3fba97b9c60e60e943c0f
anto004 <anto_004@live.com>
2018-09-05 15:20:10 -0700

    Initial Commit

---
+++ Homework8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Homework8
+//
+//  Created by Antonio Bang on 9/5/18.
+//  Copyright © 2018 UCLAExtension. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
