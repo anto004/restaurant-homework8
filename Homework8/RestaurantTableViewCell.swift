8d3fa0879cdb01ea6e5e4edf9c4450b5eccd2def
anto004 <anto_004@live.com>
2018-09-06 17:40:06 -0700

    add custom cells to table views

---
+++ Homework8/RestaurantTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  RestaurantTableViewCell.swift
+//  Homework8
+//
+//  Created by Antonio Bang on 9/6/18.
+//  Copyright © 2018 UCLAExtension. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var restaurantImage: UIImageView!
+    
+    @IBOutlet weak var restaurantName: UILabel!
+    
+    @IBOutlet weak var restaurantAddress: UILabel!
+
+}
+
---
+++ Homework8/RestaurantTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  RestaurantTableViewController.swift
+//  Homework8
+//
+//  Created by Antonio Bang on 9/6/18.
+//  Copyright © 2018 UCLAExtension. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewController: UITableViewController {
+
+
+    // MARK: - Table view data source
+
+    private func loadSamples(){
+        let image1 = UIImage(named: "ice-cream");
+        
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 3
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "Restaurant";
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as?
+                RestaurantTableViewCell
+                else {
+                    fatalError("The dequeued cell is not an instance of RestaurantTableViewCell")
+                }
+        cell.restaurantName.text = "name";
+        cell.restaurantImage.image = UIImage(named: "ice-cream");
+        cell.restaurantAddress.text = "address goes here";
+
+        return cell;
+    }
+}
